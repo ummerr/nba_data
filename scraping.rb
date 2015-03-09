c8556dae5d8310455447363bcb44e0e60382180e
Amar Singh <ummerr@gmail.com>
2015-03-09 12:02:46 -0400

    added class that can pull 82 game win loss for a team

--- scraping.rb
+++ scraping.rb
@@ -5,39 +5,26 @@ require 'open-uri'
 # HAWKS_URL = "http://www.basketball-reference.com/teams/ATL/2015_games.html"
 # page = Nokogiri::HTML(open(HAWKS_URL))
 
-page = Nokogiri::HTML(open('hawks.html'))
+class TeamResults
+  attr_accessor :page, :results
 
-rows = page.css("tr")
-p rows
+  def initialize
+    @page = Nokogiri::HTML(open('hawks.html'))
+    @results = []
+    get_rows
+  end
 
-rows[1..21].each do |row|
-  p row.css("td")[7]
+  def get_rows
+    rows = page.css("tr")
+    get_results(rows)
+  end
+
+  def get_results(rows)
+    rows[1..-1].each do |row|
+      if row.css("td")[7] != nil
+        @results << row.css("td")[7].text #row 7 is the win loss of a team
+      end
+    end
+  end
+
 end
-
-# column = rows[21].css("td")[7].text
-# p column
-
-# columns = rows[1].css("td")[7].text
-# puts columns
-# p columns.class
-
-
-#column 7 is W or L
-
-# <td align="right">1</td>
-# <td align="left" csk="2014-10-29"><a href="/boxscores/index.cgi?month=10&amp;day=29&amp;year=2014">Wed, Oct 29, 2014</a></td>
-# <td align="">7:30p <span style="font-size:7px;vertical-align:baseline;">EST</span>
-# </td>
-# <td align=""></td>
-# <td align="center"><a href="/boxscores/201410290TOR.html">Box Score</a></td>
-# <td align="center">@</td>
-# <td align="left" csk="TOR2014-10-29"><a href="/teams/TOR/2015.html">Toronto Raptors</a></td>
-# <td align="center">L</td>
-# <td align="center"></td>
-# <td align="right">102</td>
-# <td align="right">109</td>
-# <td align="right">0</td>
-# <td align="right">1</td>
-# <td align="left">L 1</td>
-# <td align="left"></td>
-# Nokogiri::XML::NodeSet
