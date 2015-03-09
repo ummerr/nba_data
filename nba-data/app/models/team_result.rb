0295c01a7d96a416a9d52c55f6f0818b214daf0f
Amar Singh <ummerr@gmail.com>
2015-03-09 15:14:09 -0400

    added team_result class

---
+++ nba-data/app/models/team_result.rb
@@ -0,0 +1,26 @@
+class TeamResult < ActiveRecord::Base
+  attr_accessor :page, :results
+
+  def initialize(team_name)
+    @page = Nokogiri::HTML(open("http://www.basketball-reference.com/teams/#{team_name}/2015_games.html"))
+    @results = []
+    get_rows
+  end
+
+  def get_rows
+    rows = page.css("tr")
+    get_results(rows)
+  end
+
+  def get_results(rows)
+    rows[1..-1].each do |row|
+      if row.css("td")[7] != nil
+        @results << row.css("td")[7].text #row[7] is the win loss of a team for a game
+      end
+    end
+  end
+
+  # HAWKS_URL = "http://www.basketball-reference.com/teams/ATL/2015_games.html"
+  # page = Nokogiri::HTML(open(HAWKS_URL))
+
+end
