f8b52bbdae439dbff40edf6889b20b7a8d2f31c2
Amar Singh <ummerr@gmail.com>
2015-03-09 15:09:33 -0400

    added a post route for form

--- nba-data/config/routes.rb
+++ nba-data/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/' => 'stats#index'
+  post '/results' => 'stats#find_results'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
